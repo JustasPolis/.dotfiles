ff617ee210ba266a22a3ab185b539dc53eeb32b6
Justin Polis <j.polikevicius@gmail.com>
2023-12-25 22:18:14 +0200

    updates

--- home.nix
+++ home.nix
@@ -17,8 +17,8 @@
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
     yq-go # yaml processer https://github.com/mikefarah/yq
-    fzf
     firefox
+    fzf
 	neovim
 	kitty
 	swaylock-effects
