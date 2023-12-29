16924a39867b7f2d9316c2197dc2a080cd68acbe
Justin Polis <j.polikevicius@gmail.com>
2023-12-29 21:58:02 +0200

    updates

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager,auto-cpufreq, ... }@inputs:
     let inherit (self) outputs;
     in {
       nixosConfigurations = {
