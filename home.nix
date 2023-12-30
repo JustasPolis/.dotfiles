6e95784511d938306f66bce4f04280167ad4e18e
Justin Polis <j.polikevicius@gmail.com>
2023-12-30 15:41:37 +0200

    updates

--- home.nix
+++ home.nix
@@ -4,7 +4,7 @@
   home.homeDirectory = "/home/justin";
   home.file.".config/hypr/hyprland.conf".source = ./hypr/hyprland.conf;
   home.file.".config/hypr/hyprpaper.conf".source = ./hypr/hyprpaper.conf;
-  home.file.".config/kitty/kitty.conf".source = ./kitty/kitty.conf;
+  home.file.".config/kitty".source = ./kitty;
   home.file.".config/wallpapers".source = ./wallpapers;
   home.file.".config/swayidle/config".source = ./swayidle/config;
   home.file.".config/swaylock/config".source = ./swaylock/config;
