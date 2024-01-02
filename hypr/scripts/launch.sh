ba8d38673d464e2ca55bdbe5bb3741e329162edd
Justin Polis <j.polikevicius@gmail.com>
2024-01-02 11:23:09 +0200

    updates

--- hypr/scripts/launch.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-hyprctl setcursor 'Bibata-Modern-Ice' 24
-hyprctl --batch 'keyword windowrulev2 workspace 3 silent, class:(firefox) ; dispatch exec firefox ; keyword windowrulev2 workspace 3, class:(firefox);'
-hyprctl --batch 'keyword windowrulev2 workspace 2 silent, class:(kitty) ; dispatch exec kitty ; keyword windowrulev2 workspace 2, class:(kitty);'
