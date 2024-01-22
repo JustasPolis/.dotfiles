f59e60e15cc268ba9dff32aa0f631ef9894c8710
Justin Polis <j.polikevicius@gmail.com>
2024-01-22 19:31:03 +0200

    fish

--- fish/functions/nix-dev-init.fish
+++ fish/functions/nix-dev-init.fish
@@ -1,4 +1,20 @@
-function nix-dev-init
-    nix flake init -t "github:the-nix-way/dev-templates#$1"
-    direnv allow
+function initialize_nix_project
+    set template $argv[1]
+
+    # Check if a language is provided
+    if test -z "$template"
+        echo "Error: Please provide a template."
+        return 1
+    end
+
+    # Run nix flake init with the specified language
+    nix flake init --template "github:the-nix-way/dev-templates#$language"
+    
+    # Check if the nix flake init command succeeded
+    if test $status -eq 0
+        direnv allow
+    else
+        echo "Error: nix flake init command failed."
+        return 1
+    end
 end
