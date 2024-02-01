5d02e9809ea835059d685da089980aebcb46ac5c
Justin Polis <j.polikevicius@gmail.com>
2024-02-01 16:21:14 +0200

    lazy lock

--- .config/nvim/lua/lazy-nvim.lua
+++ .config/nvim/lua/lazy-nvim.lua
@@ -1,14 +1,14 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -16,23 +16,24 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 local opts = {
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "matchit",
-        "netrwPlugin",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    }
-  },
-  ui = {
-    border = "rounded",
-    noautocmd = false
-  }
+	lockfile = "~/.dotfiles/.config/nvim" .. "/lazy-lock.json",
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	ui = {
+		border = "rounded",
+		noautocmd = false,
+	},
 }
 
-require("lazy").setup('plugins', opts)
+require("lazy").setup("plugins", opts)
