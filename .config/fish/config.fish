385724146e14f033fc25a2f4a802377c329718f1
Justin Polis <j.polikevicius@gmail.com>
2024-02-27 21:40:07 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,19 @@ direnv hook fish | source
 alias ls="exa --icons"
 abbr --add nixrebuild 'sudo nixos-rebuild switch --flake ~/.dotfiles/.#justin'
 set fzf_fd_opts --hidden --max-depth 3
-#alias eww="env XDG_CACHE_HOME=/dev/null eww"
 alias imgpreview="swayimg -c swayimg 2> /dev/null"
 set EDITOR nvim
 set BAT_THEME "ansi"
 set -g fish_key_bindings fish_vi_key_bindings
 
+bind -M insert \t accept-autosuggestion
+bind -M insert \cn 'commandline -f complete'
+bind -M insert \cp 'commandline -f complete-and-search'
+
 bind yy fish_clipboard_copy
 bind p fish_clipboard_paste
 zoxide init --cmd cd fish | source
+set -x PATH $PATH ~/.cargo/bin
+
+bind \cz 'fg 2>/dev/null; commandline -f repaint'
+
